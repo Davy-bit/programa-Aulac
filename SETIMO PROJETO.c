f13337046e2a7cf2111e35f413ed7e0d9c86476e
Davy-bit <davydevasconcelosmoura@gmail.com>
2024-10-07 12:39:40 -0300

    Add files via upload
    SETIMO PROJETO

---
+++ SETIMO PROJETO.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (){
+float tamanho;
+float velocidade;
+float tempo;
+printf("digite o tamanho do arquivo:");
+scanf("%f" ,&tamanho);
+printf("digite a velocidade da conexao:");
+scanf("%f" ,&velocidade);
+tempo = (tamanho/velocidade);
+printf("%f", tempo);
+
+
+       }
+    
