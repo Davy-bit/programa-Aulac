8af06e10c15380f036472696153c1dcfc2768c12
Davy-bit <davydevasconcelosmoura@gmail.com>
2024-10-07 12:07:19 -0300

    Add files via upload
    QUINTO PROJETO

---
+++ QUINTO PROJETO.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main (){
+int distancia;
+float consumo;
+float CMV;
+printf("digite a distancia percorrida:");
+scanf("%d" ,&distancia);
+printf("digite o consumo de combustivel:");
+scanf("%f" ,&consumo);
+CMV = distancia/consumo;
+printf("%f", CMV);
+
+
+       }
+    
