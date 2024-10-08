f9730d9309c1d62f668f732baf2cb38dab6cf7e1
Davy-bit <davydevasconcelosmoura@gmail.com>
2024-10-08 09:44:07 -0300

    Add files via upload
    PROJETO DE IDADES

---
+++ PROJETO DE IDADES.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main (){
+
+int idadedavy = 15;
+int idadedeivison = 17;
+
+if ("idadedavy < idadedeivison"){
+  printf("davy mais novo que deivison!");
+}
+  else printf("deivison mais velho que davy!");
+  
+}
+       
+
+
+
+
+       
+    
