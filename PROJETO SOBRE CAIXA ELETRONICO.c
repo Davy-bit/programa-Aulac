0588f0ffe28a5650546e42d0aea995cc687b4322
Davy-bit <davydevasconcelosmoura@gmail.com>
2024-10-21 12:29:37 -0300

    Add files via upload

---
+++ PROJETO SOBRE CAIXA ELETRONICO.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+int main (){
+ int numero;
+
+
+ printf("digite o numero:");
+ scanf("%d", &numero);
+
+ if(numero==1){
+  printf("saldo");
+}
+if(numero==2){
+  printf("extrato");
+}
+if(numero==3){
+  printf("saque");
+}
+if(numero==4){
+  printf("sair");
+}
+if(numero !=1 &&2 &&3 &&4){
+  printf("falha na operacao");
+}
+}
+    
+
+
+
+
+       
+    
