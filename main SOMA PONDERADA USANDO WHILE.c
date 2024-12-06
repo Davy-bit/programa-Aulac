c6c93a1b6826e041c449dce11f6965d151d6925f
Davy-bit <davydevasconcelosmoura@gmail.com>
2024-12-05 21:31:26 -0300

    Add files via upload

---
+++ main SOMA PONDERADA USANDO WHILE.c
@@ -0,0 +1,21 @@
+#include <stdio.h> 
+int main(void) { 
+
+ int n, s, p, r, d; 
+
+ printf("Numero? "); 
+ scanf("%d", &n); 
+ s = 0; 
+ p = 1; 
+
+ while( n>0 ) { 
+ r = n%10; 
+ n = n/10; 
+ s = s + r*p; 
+ p++; 
+ } 
+ 
+ d = s%10; 
+ printf("DV = %d\n", d); 
+ return 0; 
+}
